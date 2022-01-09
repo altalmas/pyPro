1be17a96a982595e3f0bb1927e2691bcb6b3c8c7
altalmas <altalmas.abdallah@gmail.com>
2022-01-09 13:18:39 +0100

    until lesson 28

---
+++ openCV/openCV17-contour-colorTracking.py
@@ -0,0 +1,91 @@
+# Lesson 27 on Youtube
+
+# Hue : 0 - 179
+# Saturation : 0 - 255
+# Value : 0 - 255
+
+import cv2
+import numpy as np
+
+def nothing():
+    pass
+
+cv2.namedWindow('Trackbars')
+
+cv2.createTrackbar('hueLow', 'Trackbars', 0, 179, nothing)
+cv2.createTrackbar('hueHigh', 'Trackbars', 100, 179, nothing)
+cv2.createTrackbar('hueLow2', 'Trackbars', 50, 179, nothing)
+cv2.createTrackbar('hueHigh2', 'Trackbars', 100, 179, nothing)
+cv2.createTrackbar('satLow', 'Trackbars', 50, 255, nothing)
+cv2.createTrackbar('satHigh', 'Trackbars', 255, 255, nothing)
+cv2.createTrackbar('valLow', 'Trackbars', 100, 255, nothing)
+cv2.createTrackbar('valHigh', 'Trackbars', 255, 255, nothing)
+
+cv2.moveWindow('Trackbars', 1320, 0)
+
+dispW = 320*2
+dispH = 240*2
+flip = 0 # or 2
+
+camSet='nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
+
+#cam = cv2.VideoCapture(camSet) # Rasperri Pi Camera
+cam = cv2.VideoCapture(1) # for webcam
+
+while True:
+    ret, frame = cam.read()    # --- Read the Frame
+    #frame = cv2.imread('smarties.png')
+                               # --- Magic is here
+
+    frame = cv2.resize(frame,(320,240))
+    hsv= cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    hueLow = cv2.getTrackbarPos('hueLow', 'Trackbars')
+    hueHigh = cv2.getTrackbarPos('hueHigh', 'Trackbars')
+    hueLow2 = cv2.getTrackbarPos('hueLow2', 'Trackbars')
+    hueHigh2 = cv2.getTrackbarPos('hueHigh2', 'Trackbars')
+
+    satLow = cv2.getTrackbarPos('satLow', 'Trackbars')
+    satHigh = cv2.getTrackbarPos('satHigh', 'Trackbars')
+
+    valLow = cv2.getTrackbarPos('valLow', 'Trackbars')
+    valHigh = cv2.getTrackbarPos('valHigh', 'Trackbars')
+
+    l_b = np.array([hueLow,satLow,valLow]) # lower bound
+    u_b = np.array([hueHigh,satHigh,valHigh]) # higher bound
+    l_b2 = np.array([hueLow2,satLow,valLow]) # lower bound
+    u_b2 = np.array([hueHigh2,satHigh,valHigh]) # higher bound
+
+    FGMask = cv2.inRange(hsv,l_b,u_b)
+    FGMask2 = cv2.inRange(hsv,l_b2, u_b2)
+    FGMaskComp = cv2.add(FGMask,FGMask2)
+    cv2.imshow('FGMaskComp', FGMaskComp)
+    cv2.moveWindow('FGMaskComp', 0,410)
+
+    contours,_ = cv2.findContours(FGMaskComp, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # now, to filter out alot of unnecessary contours, we sort the controus from the 
+    # biggest to the smallest, and then we care about the biggest only
+    contours = sorted(contours, key=lambda x:cv2.contourArea(x), reverse=True)
+
+    # if there is more than one object to capture, we filter out the contours that are unimportant
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        (x,y,w,h) = cv2.boundingRect(cnt)
+        if area >= 50:
+            #cv2.drawContours(frame, [cnt], 0, (255,0,0), 3)
+            #cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 3)
+            cv2.line(frame, (0,int(y+(h/2))), (dispW,int(y+(h/2))), (255,0,0), 3 )
+            cv2.line(frame, (int(x+(w/2)),0), (int(x+(w/2)),dispH), (255,0,0), 3 )
+
+
+    #cv2.drawContours(frame, contours, 0, (255,0,0), 3) # -1 means draw all contours
+
+    cv2.imshow('piCam', frame) # --- Show the Frame
+    cv2.moveWindow('piCam', 0, 0)
+
+
+
+    if cv2.waitKey(1)==ord('q'): # wait to press 'q'
+        break
+
+cam.release()
+cv2.destroyAllWindows()
