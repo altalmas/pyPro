697fa12c37381503e12308de3f9ff865a82e7f14
altalmas <altalmas.abdallah@gmail.com>
2022-01-17 18:38:15 +0100

    until lesson 59

---
+++ GPIO/pwm.py
@@ -0,0 +1,20 @@
+"""
+for pwm pin configurations, please see the following:
+
+    https://toptechboy.com/ai-on-the-jetson-nano-lesson-59-pwm-on-the-gpio-pins-of-the-jetson-nano/
+
+    and on youtube (the same video):
+
+    https://www.youtube.com/watch?v=eImDQ0PVu2Y 
+
+
+Also, google the following:
+    (Configuring the 40-pin expansion header)
+
+    Then, you will go to:
+
+    https://docs.nvidia.com/jetson/l4t/index.html#page/Tegra%20Linux%20Driver%20Package%20Development%20Guide/hw_setup_jetson_io.html 
+
+ """
+
+ 
